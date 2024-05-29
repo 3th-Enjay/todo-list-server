993d3e32c75a81462a55803f89da636c101d2f1c
3th-enjay <enjayjerey@gmail.com>
2024-05-29 11:29:25 +0100

    backend to do list

---
+++ services/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const baseDir = path.join(__dirname, '../to-dos');
+
+
+class todoService {
+
+    createTodo(name, data) {
+        const fileName = path.join(baseDir, name)
+    }
+}
